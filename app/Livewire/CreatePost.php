23a2ff589f2b2b5d8de81c6cbbfdad55b4b0ad15
JuanXB <bertoia114@gmail.com>
2024-05-09 13:26:50 -0300

    Properties and Data Binding

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
+    public string $title = '';
+
+    public string $body = '';
+
     public function render()
     {
         return view('livewire.create-post');
