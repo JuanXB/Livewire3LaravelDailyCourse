b84900b1c63268a403baaac4ff5b601736a7132e
JuanXB <bertoia114@gmail.com>
2024-05-09 13:37:58 -0300

    Form Validation

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -3,12 +3,15 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
+    #[Validate('required|min:5')]
     public string $title = '';
 
+    #[Validate('required|min:5')]
     public string $body = '';
 
     public function render()
@@ -18,6 +21,9 @@ class CreatePost extends Component
 
     public function save(): void
     {
+
+        $this->validate();
+        
         Post::create([ 
             'title' => $this->title,
             'body' => $this->body,
