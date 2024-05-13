8c1eab2aae86f117ff15d92ef4d852aae2fa751e
JuanXB <bertoia114@gmail.com>
2024-05-13 14:27:16 -0300

    Update When Focus Removed

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,7 +5,7 @@
     <form method="POST" wire:submit='save'>
         <div>
             <label for="title" class="block font-medium text-sm text-gray-700">Title</label>
-            <input id="title" wire:model.live="postForm.title"
+            <input id="title" wire:model.blur="postForm.title"
                 class="block text-stone-950 mt-1 w-full border-gray-300 rounded-md shadow-sm" type="text" />
             @error('postForm.title')
             <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
@@ -14,7 +14,7 @@
 
         <div class="mt-4">
             <label for="body" class="block font-medium text-sm text-gray-700">Body</label>
-            <textarea id="body" wire:model.live="postForm.body"
+            <textarea id="body" wire:model.blur="postForm.body"
                 class="block mt-1 text-stone-950 w-full border-gray-300 rounded-md shadow-sm"></textarea>
             @error('postForm.body')
             <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
